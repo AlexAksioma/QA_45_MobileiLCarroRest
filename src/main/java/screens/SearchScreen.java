87bd884871ea15126e96c28a301e7f06ce97164d
AlexAksioma <alexmedqwerty@gmail.com>
2025-02-03 19:24:54 +0200

    started find car

--- src/main/java/screens/SearchScreen.java
+++ src/main/java/screens/SearchScreen.java
@@ -2,9 +2,13 @@ package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SearchScreen extends BaseScreen{
+public class SearchScreen extends BaseScreen {
     public SearchScreen(AppiumDriver<AndroidElement> driver) {
         super(driver);
     }
@@ -18,11 +22,23 @@ public class SearchScreen extends BaseScreen{
     AndroidElement btnLogin;
     @FindBy(xpath = "//*[@text='My Cars']")
     AndroidElement btnMyCars;
+    //============================
+    @FindBy(id = "com.telran.ilcarro:id/editLocation")
+    AndroidElement inputCity;
+    @FindBy(id = "com.telran.ilcarro:id/editFrom")
+    AndroidElement inputDateFrom;
+    @FindBy(id = "com.telran.ilcarro:id/editTo")
+    AndroidElement inputDateTo;
+    @FindBy(id = "com.telran.ilcarro:id/searchBtn")
+    AndroidElement btnYalla;
+    @FindBy(id = "android:id/button1")
+    AndroidElement btnOkCalendar;
 
-    public void goToRegistrationScreen(){
+
+    public void goToRegistrationScreen() {
         //btnMoreOptions.click();
         //btnRegistration.click();
-        clickWait(btnMoreOptions,5);
+        clickWait(btnMoreOptions, 5);
         clickWait(btnRegistration, 5);
     }
 
@@ -30,12 +46,38 @@ public class SearchScreen extends BaseScreen{
         clickWait(btnMoreOptions, 5);
         clickWait(btnLogin, 5);
     }
+
     public void goToMyCarsScreen() {
         clickWait(btnMoreOptions, 5);
         clickWait(btnMyCars, 5);
     }
 
-    public boolean validateMessageSuccess(String message){
+    public boolean validateMessageSuccess(String message) {
         return textInElementPresent(popUpMessageSuccess, message, 5);
+    }
+
+    public void findCarWithCalendar(String city, String dateStart, String dateEnd) {
+        //clickWait(inputCity, 5);  //02 March 2025
+        inputCity.sendKeys(city);
+        inputDateFrom.click();
+        setDateCalendar(dateStart);
+        inputDateTo.click();
+        setDateCalendar(dateEnd);
+    }
+
+    private void setDateCalendar(String dateStart) {
+        //pause(3);
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.elementToBeClickable
+                        (By.xpath("//*[@content-desc='" + dateStart + "']")))
+                .click();
+        btnOkCalendar.click();
+        try {
+            new WebDriverWait(driver, 5)
+                    .until(ExpectedConditions.invisibilityOfElementLocated
+                            (By.id("android:id/button1")));
+        } catch (TimeoutException e){
+            e.printStackTrace();
+        }
     }
 }
